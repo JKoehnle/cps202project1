0731d8b824aa16ff6a7e2d2f2648852070ac56b6
123hsewell <122289311+123hsewell@users.noreply.github.com>
2023-03-03 09:25:17 -0500

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,11 @@
+def userint():
+  money = float(input("How much money do you have? ")) #gets amount of money and force conversion to float
+
+  try: 
+     money >= 0 #checks the amount of money is greater than 0
+  except NoMoneyError:
+     print("You have no money!")
+    return userint() #recalls the function to correct error
+
+def state():
+    proceed = input("Do you wish to buy more from the vending machine enter true or false: ") #checks user state and desires
